7081610a54d840bbc956280b46bb0f854c50f358
Tim Kalan <timkalan99@gmail.com>
2020-11-02 23:15:07 +0100

    glavni vzorec narejen

--- zajemi.py
+++ zajemi.py
@@ -26,11 +26,13 @@ def zajemi_glavne(st_strani=99):
     return vina_url
 
 
+# naštimi to da ti da v tri folderje da github pobere
 def zajemi_posamezna(seznam_urljev):
     """
     Od zgornje funkcije prejme seznam vin in pobere njih posamezne strani. Hkrati 
     dobi tudi vrsto proizvoda - ni nujno, da so samo vina.
     """
+
     i = 1
     for link, vrsta in seznam_urljev:
         orodja.shrani_spletno_stran(link[1:-1], f'zajete_strani/oglasi/oglas{i}.html')
@@ -38,7 +40,58 @@ def zajemi_posamezna(seznam_urljev):
         print(i)
 
 
-if __name__ == '__main__':
-    #zajemi_glavne()
-    #zajemi_posamezna(zajemi_glavne())
-    print(zajemi_glavne()[-1])
+# vzorec enega od 25 oglasov na strani
+vzorec_oglasa = re.compile(
+    r'<div id="product_id.*?' 
+    r'<div class="clearfix"></div>',
+    flags=re.DOTALL
+)
+
+# vzorec oglasa, ki predstavlja vino (gourmet in other spustimo)
+vzorec_vina = re.compile(
+    r'<div id="product_id_(?P<id>\d{3,8}).*?'
+    r'<a href="https://winelibrary.com/wines.*?'
+    r"<span class='js-elip-multi'>(?P<leto>\d{4})? "
+    r'(?P<ime>.*?)</span>.*?'
+    r'<small>(?P<sorta>.*?) from '
+    r'(?P<drzava>.*?)</small>.*?'
+    r'</small> &nbsp; (<small>)?(?P<velikost>\d{1,4}).*?(</small>)?</p>.*?'
+    r'<p class="h4 search-item-price">.*?\$(?P<znizana_cena>\d{1,5}\.\d{0,2}).*?'
+    r"<span class='strike'>\$(?P<cena>\d{1,5}\.\d{0,2})</span>", 
+    flags=re.DOTALL
+)
+
+
+# vzorec ocene in ocenjevalca (ni nujno, da se pojavita)
+vzorec_ocene = re.compile(
+    r'<div><span class="ptag-score">(?P<ocena>.*?)</span>.*?'
+    r'<span class="name">(?P<ocenjevalec>.*?)</span>.*?', 
+    flags=re.DOTALL
+)
+
+#vzorec_other = re.compile(
+#    r'', 
+#    flags=re.DOTALL
+#)
+#
+#
+#vzorec_gourmet = re.compile(
+#    r'', 
+#    flags=re.DOTALL
+#)
+
+
+def izloci_podatke_oglasa(oglas):
+    """
+    Izloči vse relevantne podatke iz enega od 25 oglasov, ki so na vsaki strani. 
+
+    oglas = del HTML-ja, ki predstavlja en oglas
+    """
+
+i = 0
+for oglas in re.finditer(vzorec_oglasa, orodja.vsebina_datoteke('zajete_strani/vina/vina101-125.html')):
+    if re.search(vzorec_ocene, oglas[0]):
+        print(re.search(vzorec_ocene, oglas[0]).groupdict())
+        i += 1
+
+print(i) 
